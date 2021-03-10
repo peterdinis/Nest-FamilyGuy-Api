d16bd5f256333362bf38985b4ad94458145a5289
Peterdinis <peterdinis0@pobox.sk>
2021-03-10 21:48:31 +0100

    Edit cors and remove tests

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors();
   app.use(morgan('dev'));
   await app.listen(3001);
 }
