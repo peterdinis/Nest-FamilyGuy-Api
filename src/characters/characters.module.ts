29d27f5bda3e3823873ae6543f8714084097f3b6
Peterdinis <peterdinis0@pobox.sk>
2021-03-10 13:21:15 +0100

    Fixed: problem in module

--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CharactersController } from './characters.controller';
 import { CharactersService } from './characters.service';
+import {MongooseModule} from '@nestjs/mongoose';
+import {CharacterSchema} from './character.model';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{
+      name: 'Character',
+      schema: CharacterSchema
+    }])
+  ],
   controllers: [CharactersController],
   providers: [CharactersService ],
 })
